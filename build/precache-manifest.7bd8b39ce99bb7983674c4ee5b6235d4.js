f01c15c143c5a1c4d2a4e029c4f7796a4f3f10ef
Elijah Hensel <elijah.witth@gmail.com>
2021-07-28 19:13:05 -0500

    revert

---
+++ build/precache-manifest.7bd8b39ce99bb7983674c4ee5b6235d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88bfe79724c13bda2f249a6506f0f2b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e1673b44bd717d5be0f",
+    "url": "/static/css/main.113bb285.chunk.css"
+  },
+  {
+    "revision": "b5b36a2eb1505150690c",
+    "url": "/static/js/2.3be2102b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3be2102b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1673b44bd717d5be0f",
+    "url": "/static/js/main.09c1fd85.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  },
+  {
+    "revision": "a26ef88148eacf80a0fd440ba72720e7",
+    "url": "/static/media/notepad.a26ef881.jpg"
+  }
+]);
